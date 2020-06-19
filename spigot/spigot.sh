7d26d36396096c8d4547f6eb1b0944ad97fc6efb
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-06-19 15:31:24 +0200

    [FEAT] Now, the server.properties file is atomatically configured by the .env file variables
    Former-commit-id: b94df1d4afd75c973ec1e0c84569a3a725e96a4b

--- spigot/spigot.sh
+++ spigot/spigot.sh
@@ -39,11 +39,55 @@ function setServerProp {
   fi
 }
 
+
+
+
 ls -al $SPIGOTDIR
+setServerProp "broadcast-rcon-to-ops" "$BROADCAST_RCON_TO_OPS"
+setServerProp "view-distance" "$VIEW_DISTANCE"
+setServerProp "max-build-height" "$MAX_BUILD_HEIGHT"
+setServerProp "server-ip" "$SERVER_IP"
+setServerProp "level-seed" "$LEVEL_SEED"
+setServerProp "rcon.port" "$RCON_PORT"
+setServerProp "gamemode" "$GAMEMODE"
+setServerProp "server-port" "$SERVER_PORT"
+setServerProp "allow-nether" "$ALLOW_NETHER"
+setServerProp "enable-command-block" "$ENABLE_COMMAND_BLOCK"
+setServerProp "enable-rcon" "$ENABLE_RCON"
+setServerProp "enable-query" "$ENABLE_QUERY"
+setServerProp "op-permission-level" "$OP_PERMISSION_LEVEL"
+setServerProp "prevent-proxy-connections" "$PREVENT_PROXY_CONNECTIONS"
+setServerProp "generator-settings" "$GENERATOR_SETTINGS"
+setServerProp "resource-pack" "$RESOURCE_PACK"
+setServerProp "level-name" "$LEVEL_NAME"
+setServerProp "rcon.password" "$RCON_PASSWORD"
+setServerProp "player-idle-timeout" "$PLAYER_IDLE_TIMEOUT"
 setServerProp "motd" "$MOTD"
-
-
-
+setServerProp "query.port" "$QUERY_PORT"
+setServerProp "debug" "$DEBUG"
+setServerProp "force-gamemode" "$FORCE_GAMEMODE"
+setServerProp "hardcore" "$HARDCORE"
+setServerProp "white-list" "$WHITE_LIST"
+setServerProp "broadcast-console-to-ops" "$BROADCAST_CONSOLE_TO_OPS"
+setServerProp "pvp" "$PVP"
+setServerProp "spawn-npcs" "$SPAWN_NPCS"
+setServerProp "generate-structures" "$GENERATE_STRUCTURES"
+setServerProp "spawn-animals" "$SPAWN_ANIMALS"
+setServerProp "snooper-enabled" "$SNOOPER_ENABLED"
+setServerProp "difficulty" "$DIFFICULTY"
+setServerProp "function-permission-level" "$FUNCTION_PERMISSION_LEVEL"
+setServerProp "network-compression-threshold" "$NETWORK_COMPRESSION_THRESHOLD"
+setServerProp "level-type" "$LEVEL_TYPE"
+setServerProp "spawn-monsters" "$SPAWN_MONSTERS"
+setServerProp "max-tick-time" "$MAX_TICK_TIME"
+setServerProp "enforce-whitelist" "$ENFORCE_WHITELIST"
+setServerProp "use-native-transport" "$USE_NATIVE_TRANSPORT"
+setServerProp "max-players" "$MAX_PLAYERS"
+setServerProp "resource-pack-sha1" "$RESOURCE_PACK_SHA1"
+setServerProp "spawn-protection" "$SPAWN_PROTECTION"
+setServerProp "online-mode" "$ONLINE_MODE"
+setServerProp "allow-flight" "$ALLOW_FLIGHT"
+setServerProp "max-world-size" "$MAX_WORLD_SIZE"
 
 
 
