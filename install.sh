8e80696e660fe0ab3e3cde427e4c31b3fb2f9445
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-06-19 16:35:37 +0200

    [install.sh] check_requirements

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # See https://github.com/SaphireVert/scalewayMinecraftServ/issues/10
 # TODO:
 # [ ] Add a welcome header
-# [ ] Check that all dependencies are installed, e.g. tmux, git, etc...
 # [ ] Clone this repo and cd into dir (see https://unix.stackexchange.com/a/97922)
 # [ ] cp .env_sample to .env
 # [ ] ask question to change the .env file (wizard)
+# [x] Check that all dependencies are installed, e.g. tmux, git, etc...
 # [ ] finally, launch the server in a tmux process (see https://unix.stackexchange.com/questions/22682/how-to-launch-a-set-of-program-inside-tmux-or-gnome-terminal-within-a-script etc.)
 #
 
@@ -22,3 +22,16 @@ function header {
 }
 
 header
+
+function check_requirements {
+  for app in "$@"
+  do
+    if ! [[ "$(command -v $app)" ]]; then
+      echo -e "\e[31mWARNING:\e[39m $app is not installed";
+      echo -e "\e[34mCOMMAND:\e[39m sudo apt install $app";
+      exit 1;
+    fi
+  done
+}
+
+check_requirements git tmux docker docker-compose
