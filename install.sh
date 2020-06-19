b4ead17b682074f655fc24ff12044173811c717e
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-06-19 16:37:56 +0200

    [install.sh] ensure_dotenv_present
    Former-commit-id: b01f869f1c1487fe6802cfc2494fec62fe7aee45

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # See https://github.com/SaphireVert/scalewayMinecraftServ/issues/10
 # TODO:
 # [ ] Add a welcome header
-# [ ] cp .env_sample to .env
 # [ ] ask question to change the .env file (wizard)
 # [x] Check that all dependencies are installed, e.g. tmux, git, etc...
 # [x] Clone this repo and cd into dir (see https://unix.stackexchange.com/a/97922)
+# [x] cp .env_sample to .env
 # [ ] finally, launch the server in a tmux process (see https://unix.stackexchange.com/questions/22682/how-to-launch-a-set-of-program-inside-tmux-or-gnome-terminal-within-a-script etc.)
 #
 
@@ -46,3 +46,13 @@ function clone_repos {
   fi
 }
 clone_repos https://github.com/saphirevert/scalewayMinecraftServ
+
+function ensure_dotenv_present {
+  if ! [[ -f ".env" ]]; then
+    echo "Creating the file '.env' based on '.env_sample'"
+    cp .env_sample .env
+  else
+    echo "The file '.env' is already present"
+  fi
+}
+ensure_dotenv_present
