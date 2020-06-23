18f600fffffb3f4a88a848efa7b1a642316f8a0b
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-06-23 13:25:16 +0200

    Ne fonctionne toujours pas...

--- library.sh
+++ library.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "Script started"
-echo "$1"
 
 
 function yesNoPrompt {
-  read -p "$3" -n 1 -r
+  read -p "$2" -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
     # Yes
     echo "Yes"
-    $("$2")
+    return 1
   else
     # No
     echo "No"
+    return 0
   fi
 }
 
-function makeReset {
-  echo "reset en cours"
-  # make clean
-  # make build
-  # make up
+function yesNoReset {
+  value=$(yesNoPrompt)
+  echo "jsfih"
+  echo "$value"
+  if [[ $value == 1 ]]
+  then
+    make clean
+    make build
+    make up
+  fi
 }
-
-$("$2")
 
 if [[ "$1" == "script" ]]
 then
   echo "Perfect !"
 fi
 
-if [[ "$1" == "yesNoPrompt" ]]
+if [[ "$1" == "yesNoReset" ]]
 then
-  yesNoPrompt $2
+  echo "$2"
+  yesNoReset
 fi
 
 
