92ab538e10db6ad281a75841b8665e8df69d5e96
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-06-23 13:35:26 +0200

    [FEAT] Makefile Reset - Add a confirmation Easy solution... Finally decided to create new "yesNoReset" funtion in library.sh without good factoring
    Former-commit-id: a49aa8459b0d39f54c0a1aaaf15f095a224d67df

--- library.sh
+++ library.sh
@@ -12,23 +12,25 @@ function yesNoPrompt {
   then
     # Yes
     echo "Yes"
-    return 1
   else
     # No
     echo "No"
-    return 0
   fi
 }
 
 function yesNoReset {
-  value=$(yesNoPrompt)
-  echo "jsfih"
-  echo "$value"
-  if [[ $value == 1 ]]
+  read -p "$2" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
   then
+    # Yes
+    echo "Yes"
     make clean
     make build
     make up
+  else
+    # No
+    echo "No"
   fi
 }
 
